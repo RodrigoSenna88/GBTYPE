b4b3713466f9169fda992241faabfe5b7ea78b24
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-06-26 19:39:59 -0300

    Fazendo repositório pra token com erro no método

---
+++ src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,23 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
+import UserToken from '../entities/User';
+
+class UserTokensRepository implements IUserTokensRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.ormRepository.findOne({
+      where: { token },
+    });
+
+    return userToken;
+  }
+}
+
+export default UserTokensRepository;
