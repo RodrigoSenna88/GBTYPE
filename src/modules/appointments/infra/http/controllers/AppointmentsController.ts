2f232518fc7d655daef2b1737a052f272a63a8d5
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-07-27 20:52:29 -0300

    configurando cache para agendamento de dia específico de um prestador específico

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -11,14 +10,12 @@ export default class AppointmentsController {
 
     // para criar agendamentos em horas (zerando os minutos, segundos, milisegundos)
 
-    const parsedDate = parseISO(date);
-
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
       provider_id,
       user_id,
-      date: parsedDate,
+      date,
     });
 
     return response.json(appointment);
