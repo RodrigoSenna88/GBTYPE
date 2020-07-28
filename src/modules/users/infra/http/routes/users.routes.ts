1456823949e8a2527021a18ab2aa9e6604849027
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-07-28 15:40:53 -0300

    acertando upload config, salvando arquivo no s3 corretamente agora

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -12,7 +12,8 @@ import ensureAuthenticated from '../middlewares/ensureAuthtenticated';
 const usersRouter = Router();
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
-const upload = multer(uploadConfig.config.disk);
+
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post(
   '/',
